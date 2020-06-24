8ec3c5eb860a4302094414353f0ebe6c672b28bd
wangjiade <wangjiade@oppo.com>
2020-06-24 16:31:56 +0800

    [提交完整性(X/N)][1/1]{字符串逻辑} 适用范围：{无} 准入id：{无} 分析：{无} 方案：{无} 风险及影响[快/稳/省/功能/安全隐私]：{无} 测试建议：{无} 跨组依赖(topic name)：{无}

---
+++ fundation/Operator.py
@@ -0,0 +1,35 @@
+#次方：
+a = 3
+b = 4
+print(a ** b)
+
+# 取商
+print(10 // 3)
+
+# 求余
+print(10 % 2)
+
+#不等于
+print( 3 != 5)
+
+
+
+# 逻辑运算符
+"""
+and   or   not
+
+x and y :  x为真，值是y,x为假，值是x
+x or y :   x为真，值就是x，x为假，值是y
+
+
+1.在没有（）的情况下 ， 优先级：not > and > or
+2.同一优先级从左到右依次计算
+"""
+
+print(1 < 3 and 3 < 4 or 1>2)
+
+#成员运算符
+
+# in 和 not in
+print('123' in '123333333333')
+print('abc' not in '123333333333')
