31e603f2a4e4c87bd153bcc8d5e74d92d5278512
wangjiade <wangjiade@oppo.com>
2020-06-28 18:11:15 +0800

    [提交完整性(X/N)][1/1]{while循环练习补充} 适用范围：{无} 准入id：{无} 分析：{无} 方案：{无} 风险及影响[快/稳/省/功能/安全隐私]：{无} 测试建议：{无} 跨组依赖(topic name)：{无}

--- fundation/while.py
+++ fundation/while.py
@@ -91,3 +91,74 @@ while count <= 3:
     else:
         print("fucking code Error!")
     count = count + 1
+
+
+# 利用while语句写出猜大小的游戏：
+# 设定一个理想数字比如：50，让用户输入数字，如果比50大，则显示猜测的结果大了；如果比50小，则显示猜测的结果小了;只有等于50，显示猜测结果，然后退出循环。
+# 给用户三次猜测机会，如果三次之内猜测对了，则显示猜测正确，退出循环，如果三次之内没有猜测正确，则自动退出循环，并显示‘???’。
+count = 1
+while count < 4:
+    num = int(input("please fucking input:"))
+    if num < 50:
+        print("fucking small!")
+    elif num > 50:
+        print("fucking bigger!")
+    else:
+        print("what the fucking lucky!")
+        break
+    count += 1
+else:
+    print("？？？")
+
+
+# while循环 1 2 3 4 5 6 7 9 10
+sb = 1
+while sb < 11:
+    if sb == 8:
+        pass
+    else:
+        print(sb)
+    sb += 1
+# method2：
+zz = 1
+while zz < 11:
+    if zz == 8:
+        zz += 1
+    print(zz)
+    zz += 1
+
+
+
+# 求1-100的所有数的和
+sb =0
+mdzz = 1
+while  mdzz < 101:
+    sb = sb + mdzz
+    if mdzz == 100:
+        print(sb)
+    mdzz += 1
+
+# 输出 1-100 内的所有奇数,输出 1-100 内的所有偶数
+zz = 0
+while zz < 101:
+    zz += 1
+    if zz % 2 == 0:
+        print(zz)
+
+sb = 0
+while sb < 100:
+    sb += 1
+    if sb % 2 != 0:
+        print(sb)
+
+
+# 求1-2+3-4+5 ... 99的所有数的和
+sum = 0
+sb = 0
+while sb < 99:
+    sb += 1
+    if sb %2 ==0:
+        sum -= sb
+    elif sb % 2 != 0:
+        sum += sb
+print(sum)
