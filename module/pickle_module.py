33e513c153ed2c56217e2f06ab2b4029669936b1
ralph <515002119@qq.com>
2020-07-30 21:11:11 +0800

    [python][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{python pickle面向对象相关补充}
    风险及影响[快/稳/省/功能]：{无}
    测试建议：{无需测试}
    跨组依赖(提交链接)：{无}
    移植适用范围：{无}

--- module/pickle_module.py
+++ module/pickle_module.py
@@ -1,7 +1,32 @@
 '''
 @Author: Ralph
 @Date: 2020-07-23 13:36:48
-@LastEditTime: 2020-07-23 13:36:49
-@FilePath: \python\workbeach\workbeach\module\pickle_module.py
-@Effect: do
+@LastEditTime: 2020-07-30 21:09:39
+@FilePath: \workbeach\module\pickle_module.py
+@Effect: pickle 模块 和面向对象的补充
 '''
+import pickle
+
+
+class Person:
+    def __init__(self, name, age, id):
+        self.name = name
+        self.age = age
+        self.id = id
+
+
+tom = Person('tom', '20', '00001')
+sali = Person('sali', '21', '00002')
+ralph = Person('ralph', '22', '00003')
+
+# with open('pickle_file', 'wb') as f:
+#     pickle.dump(tom, f)
+#     pickle.dump(sali, f)
+#     pickle.dump(ralph, f)
+with open('pickle_file', 'rb') as f:
+    while True:
+        try:
+            ret = pickle.load(f)
+            print(ret.__dict__)  # {'name': 'tom', 'age': '20', 'id': '00001'}
+        except EOFError:
+            break
