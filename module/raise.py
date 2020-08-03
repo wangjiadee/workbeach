f288c17439a25d551c7b485dc8f43f267f30e160
ralph <515002119@qq.com>
2020-08-03 20:23:03 +0800

    [python][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{python 主动抛出异常模块}
    风险及影响[快/稳/省/功能]：{无}
    测试建议：{无需测试}
    跨组依赖(提交链接)：{无}
    移植适用范围：{无}

---
+++ module/raise.py
@@ -0,0 +1,12 @@
+'''
+@Author: Ralph
+@Type_file: Python
+@Date: 2020-08-03 20:22:26
+@LastEditTime: 2020-08-03 20:22:40
+@FilePath: \workbeach\module\raise.py
+@Effect: raise 主动抛出异常
+'''
+raise ValueError
+raise IndexError
+raise KeyError('提示信息')
+raise NotImplementedError('你没有按照要求实现函数')
