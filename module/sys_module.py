0659e1d2941fe0d3fde3c6dff3bf226a8e12c8ed
wangjiade <wangjiade@oppo.com>
2020-08-05 17:21:31 +0800

    [python][ralph.Build][1/1][BugId:N/A][无风险][无依赖][自然集成]{sys模块 Python2 补充}
    风险及影响[快/稳/省/功能]：{无}
    测试建议：{无需测试}
    跨组依赖(提交链接)：{无}
    移植适用范围：{无}

--- module/sys_module.py
+++ module/sys_module.py
@@ -21,11 +21,17 @@ sys.platform       #返回操作系统平台名称
 
 
 
+# python2 解决编码的问题
 """
-python 检查异常处理和状态
-"""
+python在安装时，默认的编码是ascii，当程序中出现非ascii编码时，python的处理常常会报错
+UnicodeDecodeError: 'ascii' codec can't decode byte 0x?? in position 1: ordinal not in range(128)，
+python没办法处理非ascii编码的，此时需要自己设置python的默认编码，一般设置为utf8的编码格式。
 
-try:
-    sys.exit(1)
-except SystemExit as e:
-    print(e)
+以下是python2的写法，但是在python3中这个需要已经不存在了，这么做也不会什么实际意义。
+在Python2.x中由于str和byte之间没有明显区别，经常要依赖于defaultencoding来做转换。
+在python3中有了明确的str和byte类型区别，从一种类型转换成另一种类型要显式指定encoding。
+"""
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
+
