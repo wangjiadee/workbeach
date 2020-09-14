52792557c31dd8b3af16e7947e1a5590bbe65a30
wangjiade <515002119@qq.com>
2020-09-14 17:10:49 +0800

    包的导入

---
+++ module/import/demo.py
@@ -0,0 +1,15 @@
+import glance
+import sys
+print(sys.path)
+print(glance)
+
+#导入一个包 就是导入一个文件 相当于执行了这个包下面的__init__文件
+#要想直接导入某个包或者文件  
+#f1： import glance.api.policy as policy
+#f2 from glance.api import policy
+
+
+# import 后面不能带。
+
+
+
